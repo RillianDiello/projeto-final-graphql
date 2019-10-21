7f38f60ae5a8b162fabe58cd634d19615f3c3579
RillianDiello <rilliansf@gmail.com>
2019-10-21 17:03:44 -0400

    Using contexto to validate user access

--- config/context.js
+++ config/context.js
@@ -1,9 +1,47 @@
+const jwt = require('jwt-simple')
+
+
+
 module.exports = async ({ req }) => {
    //Isso somente em Dev
    await require('./simularUsuarioLogado')(req)
 
    const auth = req.headers.authorization
-   console.log(auth)
+   const token = auth && auth.substring(7)
+
+   let usuario = null
+   let admin = null
+
+   //Se houver token eu faço as validações necessárias
+   if(token){
+         try{
+         let conteudoToken = jwt.decode(token, process.env.APP_AUTH_SECRET)
+
+         if(new Date(conteudoToken.exp * 1000) > new Date()){
+            usuario = conteudoToken
+         }
+      }catch(e){
+         // Token Invalido
+      }
+   }
+
+   if(usuario && usuario.perfis){
+      admin = usuario.perfis.includes('admin')
+   }
+
+   const err = new Error("Acesso Negado!")
+
+   return {
+      usuario,
+      admin,
+      validarUsuario(){
+         if(!usuario) throw err //Quando vc cria um erro com new ele não dispara, apenas com o throw (lancar)
+      },
+      validarAdmin(){
+         if(!admin) throw err
+      }
+   }
+   
 }
 
 
