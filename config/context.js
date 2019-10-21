4c9d1611d48857d7b5881e227466c16fc7a7e41f
RillianDiello <rilliansf@gmail.com>
2019-10-21 16:01:01 -0400

    User Login Simulation

--- config/context.js
+++ config/context.js
@@ -1,4 +1,7 @@
-module.exports = ({ req }) => {
+module.exports = async ({ req }) => {
+   //Isso somente em Dev
+   await require('./simularUsuarioLogado')(req)
+
    const auth = req.headers.authorization
    console.log(auth)
 }
